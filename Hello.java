0682f44cb8dd8637b584934fd97835edf09b5f54
lucas1124 <123492978+lucas1124@users.noreply.github.com>
2023-01-24 12:44:56 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,35 @@
+/*import java.util.Scanner;
+
+public class Hello{
+
+    public static void main(String[] args) throws Exception{
+       float grade;
+       char letter = 'A' ;
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input a grade between 0 and 100");
+        grade = scanner.nextFloat();
+        while (grade < 0 || grade > 100) {
+            System.out.println("Grade must be between 0 and 100. Please input again");
+            grade = scanner.nextFloat();
+        }
+        scanner.close();
+
+         if (grade >= 90 && grade <= 100){
+            letter = 'A';
+         }
+
+         else if  (grade >= 80 && grade < 90)
+            letter = 'B';
+         else if (grade >= 70 && grade < 80)
+            letter = 'C';
+         else if (grade >= 60 && grade < 70)
+            letter = 'D';
+         else if (grade >= 0 && grade < 60)
+            letter = 'F';
+
+         System.out.println("Letter = " + letter);
+         System.out.println("Grade = " + grade);
+      }
+   }
+   */
+
